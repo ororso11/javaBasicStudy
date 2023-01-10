29cda82abd9d8c74a141e8b8b6163d7715f702e7
choeyeongjae <nhd5913@naver.com>
2023-01-11 00:43:46 +0900

    중복된 코드를 메소드로 처리하는 강의

---
+++ src/Gugudan.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Gugudan {
+    // 자바 지기 박재성 님 인프런 교육
+    public static int[] cal(int times) {
+        int[] res = new int[9];
+        for(int i = 0; i < res.length;i++) {
+            res[i] = times * ( i + 1);
+        }
+        return res;
+    }
+
+    public static void print(int[] res) {
+        for (int i = 0; i < res.length;i++) {
+            System.out.println(res[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i = 2; i < 10;i++) {
+            int[] resa = cal(i);
+            print(resa);
+        }
+    }
+}
