6ed2635a5b963dec420b9446a9311504b83a1acd
ororso11 <ororso135055@gmail.com>
2023-01-12 00:50:46 +0900

    구구단 클래스화로 로직 변경

---
+++ src/GugudanClass.java
@@ -0,0 +1,19 @@
+public class GugudanClass {
+
+    public static int[] cal( int i1, int i2, int i3 ) {
+        int[] resa = new int[i2];
+
+        for(int i = 0; i <= i3;i++) {
+            resa[i] = i1 * (i + 1);
+        }
+        return resa;
+    }
+
+    public static void print(int[] resultNum) {
+        for (int i = 0; i < resultNum.length;i++) {
+            System.out.println(resultNum[i]);
+        }
+    }
+
+
+}
---
+++ src/GugudanMain01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GugudanMain01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] inputString = scanner.next().split(",");
+        int num1 = Integer.parseInt(inputString[0]);
+        int num2 = Integer.parseInt(inputString[1]);
+
+        for(int i = 2; i <= num1; i++) {
+            int[] res = GugudanClass.cal(i,num1,num2);
+            GugudanClass.print(res);
+        }
+
+    }
+}
