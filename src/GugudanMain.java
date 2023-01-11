f7688a524b2684f5ead5656be7b56303a18b037f
ororso11 <ororso135055@gmail.com>
2023-01-11 23:28:49 +0900

    최종미션 구현을 위해 기존파일 제거

--- src/GugudanMain.java
+++
@@ -1,8 +0,0 @@
-public class GugudanMain {
-    public static void main(String[] args) {
-        for (int i = 0; i < 11;i++) {
-            int[] res = Gugudan.cal(i);
-            Gugudan.print(res);
-        }
-    }
-}
