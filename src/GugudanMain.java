7d3b515bf9b3448ae33fb98722c4bb0ef3649128
ororso11 <ororso135055@gmail.com>
2023-01-11 23:19:19 +0900

    중복된 코드를 메소드로 처리하는 강의 02

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,6 +1,6 @@
 public class GugudanMain {
     public static void main(String[] args) {
-        for (int i = 0; i < 10;i++) {
+        for (int i = 0; i < 11;i++) {
             int[] res = Gugudan.cal(i);
             Gugudan.print(res);
         }
